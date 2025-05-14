cb751d0f54636040615f0fd9d65db76dcdaab596
Unweared <129084959+Unweared@users.noreply.github.com>
2025-05-14 16:55:44 +1000

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ def create_app():
     
     return app
 
-if __name__ == '__main__':
-    app = create_app()
-    
-    # Настроить приложение, чтобы оно работало на localhost и определенном порту
-    app.run(host='localhost', port=8080, debug=True)  # Здесь указываем хост и порт
+  if __name__ == '__main__':
+      app = create_app()
+      app.run(host='0.0.0.0', port=5000, debug=True)
+
